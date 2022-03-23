342902be509efd920543a65a818de09a1a8336cf
erikturkovic <eturkovic@student.unipu.hr>
2022-03-23 17:10:38 +0100

    Gotova "home" stranica
    Vidiju se commitovi i imaju link na detalje ali detalji jos nisu uređeni

--- src/views/CommitView.vue
+++ src/views/CommitView.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="home">
-
+    <ul>
+      <li v-for="item in commits" v-bind:key="item.sha">
+        Commit ->  <a href="/about">{{item.sha}}</a>
+      </li>
+      </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import ShowCommits from '@/components/ShowCommits.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import ShowCommits from "@/components/ShowCommits.vue";
 
 export default {
-  name: 'HomeView',
-  async mounted(){
+  name: "HomeView",
+  data: function () {
+    return {
+      commits: [],
+    };
+  },
+  async mounted() {
+    let rezultat = await fetch(
+      "https://api.github.com/repos/vuejs/vue/commits"
+    );
 
-    let rezultat = await fetch("https://api.github.com/repos/vuejs/vue/commits");
-
-    let podaci = await rezultat.json()
+    let podaci = await rezultat.json();
 
     console.log(podaci);
 
+    this.commits = podaci;
   },
 };
 </script>
